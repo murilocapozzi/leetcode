4e6c77a9bc8828e2903e310fda17437eeb575cf4
Murilo Capozzi <murilocapozzi1@gmail.com>
2024-06-20 12:36:42 -0300

    LeetCode-75 19/06/24

--- leetcode-75/1207/Solution.py
+++ leetcode-75/1207/Solution.py
@@ -1,33 +1,18 @@
 class Solution:
     def uniqueOccurrences(self, arr: list[int]) -> bool:
         
-        uniqueArr = set(arr)
-        lenght = len(set(arr))
-        
+        # Dicionário que irá guardar unicamente quantas vezes cada número repetiu
         occurrences = {}
-        
-        for num in arr:
-            if num in occurrences:
-                occurrences[num] += 1
-            else:
-                occurrences[num] = 1
-                
+        # Lista que irá conter a quantidade única. Caso repita, não é único e retorna falso
         quantity = []      
         
+        for num in arr:
+            if num in occurrences: occurrences[num] += 1
+            else: occurrences[num] = 1
+        
         for key in occurrences:
-            
-            if occurrences[key] in quantity:
-                return False
-            
+            if occurrences[key] in quantity: return False
             quantity.append(occurrences[key])
             
-        print(occurrences)
-        print(quantity)
-            
+        # Se na lista não entrou nenhuma quantidade repetida, o número de ocorrências é único, retorna verdadeiro
         return True
-    
-arr = [1, 2]
-
-output = Solution().uniqueOccurrences(arr)
-
-print(output)
